76c9a492328fb3abab9e620184e09926fa1fcc0b
Giacomo Terreni <giacomoterreni@gmail.com>
2020-03-07 15:51:42 +0100

    improved tests and improved robustness of enivronmentFileDomain method

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -2,9 +2,6 @@
 
 namespace Gecche\Multidomain\Foundation;
 
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Cookie;
-
 class Application extends \Illuminate\Foundation\Application
 {
     /**
@@ -84,7 +81,12 @@ class Application extends \Illuminate\Foundation\Application
     public function environmentFileDomain($domain = null)
     {
         if (is_null($domain)) {
-            $domain = $this['domain'];
+            try {
+                $domain = $this['domain'];
+            } catch (\Exception $e) {
+                $this->detectDomain();
+                $domain = $this['domain'];
+            }
         }
         $filePath = rtrim($this['path.base'], DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
         $file = '.env.' . $domain;
--- tests/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ tests/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -13,6 +13,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(191);
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
