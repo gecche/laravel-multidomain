820f561e47e2e94e56d827c402970f61201233db
Giacomo Terreni <giacomoterreni@gmail.com>
2020-04-18 16:08:35 +0200

    decommented tests

--- tests/src/ArtisanTestCase.php
+++ tests/src/ArtisanTestCase.php
@@ -177,208 +177,208 @@ class ArtisanTestCase extends TestCase
         return;
 
     }
-//
-//    /*
-//    * TEST FOR QUEUE COMMANDS
-//    * In this test we check that queues with the database driver refer to the correct db depending upon the domain.
-//    * We use for simplicity the queue:flush command.
-//    * The default .env file containts QUEUE_DRIVER=database and DB_DATABASE=homestead
-//    * First we create a new domain site1.test and we update his .env with DB_DATABASE=site1 and we reset and run the migrations
-//    * in the homestead db.
-//    * Then, we check that the queue:flush command launched without options displays the standard success message
-//    * "All failed jobs deleted successfully!" and that the queue:flush command launched with the --domain=site1.test
-//    * option fails because it doesn't find the failed_job table as we did'nt migrate for that db.
-//    * Last, we repeat the check with the other db.
-//    *
-//    */
-//    public function testQueueCommand() {
-//        //Note that if the $_SERVER['SERVER_NAME'] value has been set and the --domain option has NOT been set,
-//        //the $_SERVER['SERVER_NAME'] value acts as the --domain option value.
-//        // So all the artisan commands run as if the option were instantiated.
-//        $serverName = Arr::get($_SERVER,'SERVER_NAME');
-//
-//
-//        //ADDING DOMAIN AND UPDATING ENV
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:add site1.test');
-//        $process->run();
-//
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:update_env site1.test --domain_values=\'{"DB_DATABASE":"site1"}\'');
-//        $process->run();
-//
-//
-//        //RESET MIGRATIONS IN BOTH DBS
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset');
-//        $process->run();
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset --domain=site1.test');
-//        $process->run();
-//
-//        //MIGRATIONS WITHOUT DOMAIN OPTION
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate');
-//        $process->run();
-////        $this->assertEquals("Laravel",$process->getOutput());
-//
-//
-//        //CHECK QUEUE:FLUSH COMMAND: SUCCESS WITHOUT OPTIONS AND FAILURE WITH DOMAIN OPTION
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush');
-//        $process->run();
-//        $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
-//
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush --domain=site1.test');
-//        $process->run();
-//        if (in_array($serverName, ['site1.test']) || Str::endsWith($serverName,'.site1.test')) {
-//            $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
-//        } else {
-//            $this->assertContains('SQLSTATE[42S02]: Base table or view not found: 1146 Table \'site1.failed',$process->getOutput());
-//        }
-//
-//
-//
-//        //RESET MIGRATIONS IN BOTH DBS
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset');
-//        $process->run();
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset --domain=site1.test');
-//        $process->run();
-//
-//        //MIGRATIONS WITH DOMAIN OPTION
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate --domain=site1.test');
-//        $process->run();
-////        $this->assertEquals("Laravel",$process->getOutput());
-//
-//        //CHECK QUEUE:FLUSH COMMAND: SUCCESS WITH DOMAIN OPTIION AND FAILURE WITHOUT
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush --domain=site1.test');
-//        $process->run();
-//        $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
-//
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush');
-//        $process->run();
-//        if (in_array($serverName, ['site1.test']) || Str::endsWith($serverName,'.site1.test')) {
-//            $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
-//        } else {
-//            $this->assertContains('SQLSTATE[42S02]: Base table or view not found: 1146 Table \'homestead.failed',$process->getOutput());
-//        }
-//
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:remove site1.test --force');
-//        $process->run();
-//
-//        return;
-//
-//    }
-//
-//    /*
-//       * TEST FOR QUEUE LISTEN COMMAND
-//       * In this test we check that queues listeners applied to distinct domains work with the jobs pushed from
-//       * the corresponding domain and that the queues where they are pushed are those configured in the .env files.
-//       *
-//       * The job Gecche\Multidomain\Tests\Jobs\AppNameJob simply put in a file the name of the queue and the env(APP_NAME) value.
-//       * We push that job using an artisan Gecche\Multidomain\Tests\Console\Commands\QueuePush (queue_push) command which simply push the job in the queue.
-//       *
-//       */
-//    public function testQueueListenCommand() {
-//
-//
-//        //Note that if the $_SERVER['SERVER_NAME'] value has been set and the --domain option has NOT been set,
-//        //the $_SERVER['SERVER_NAME'] value acts as the --domain option value.
-//        // So all the artisan commands run as if the option were instantiated.
-//        $serverName = Arr::get($_SERVER,'SERVER_NAME');
-//
-//
-//        //ADDING DOMAIN AND UPDATING ENV
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:add site1.test');
-//        $process->run();
-//
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:update_env site1.test --domain_values=\'{"APP_NAME":"LARAVELTEST","DB_DATABASE":"site1","QUEUE_DEFAULT":"site1"}\'');
-//        $process->run();
-//
-//
-//        //RESET MIGRATIONS IN BOTH DBS
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset');
-//        $process->run();
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset --domain=site1.test');
-//        $process->run();
-//
-//        //MIGRATIONS WITHOUT DOMAIN OPTION
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate');
-//        $process->run();
-//
-//
-//        //This the file used by the AppNameJob job
-//        $fileToTest = $this->laravelAppPath.'/queueresult.txt';
-//
-//        $this->files->delete($fileToTest);
-//
-//        $this->assertFileNotExists($fileToTest);
-//
-//        //We start the listener
-//        $processName = "php ".$this->laravelAppPath. "/artisan queue:listen";
-//        $process = new Process($processName);
-//        $process->start();
-//
-//        //We push the Job using the queue_push command
-//        $process2 = new Process('php '.$this->laravelAppPath.'/artisan queue_push');
-//        $process2->run();
-//
-//        //Wait for a reasonable time (the only way we managed to simulate queue listeners)
-//        sleep(5);
-//
-//        //We assert that the file exists
-//        $this->assertFileExists($fileToTest);
-//
-//        //Depending upon the domain option (or the SERVER_NAME value)
-//        //we check accordingly the contents of the file
-//        $fileContent = $this->files->get($fileToTest);
-//        if (in_array($serverName, ['site1.test']) || Str::endsWith($serverName,'.site1.test')) {
-//            $this->assertContains('LARAVELTEST --- site1',$fileContent);
-//        } else {
-//            $this->assertContains('Laravel --- default',$fileContent);
-//        }
-//
-//
-//        $process->stop(0);
-//
-//        //We kill the listener process by name
-//        $string = 'pkill -f "' . $processName . '"';
-//        exec($string);
-//
-//
-//        /*
-//         * We repeat the stuff under the domain "site1.test"
-//         */
-//        /***/
-//        //MIGRATIONS WITH DOMAIN OPTION
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate --domain=site1.test');
-//        $process->run();
-//
-//        $this->files->delete($fileToTest);
-//
-//        $this->assertFileNotExists($fileToTest);
-//
-//        $processName = 'php '.$this->laravelAppPath.'/artisan queue:listen --domain=site1.test';
-//        $process = new Process($processName);
-//        $process->start();
-//
-//        $process2 = new Process('php '.$this->laravelAppPath.'/artisan queue_push --domain=site1.test');
-//        $process2->run();
-//
-//        //Wait for a reasonable time
-//        sleep(5);
-//
-//        $this->assertFileExists($fileToTest);
-//
-//        $fileContent = $this->files->get($fileToTest);
-//        $this->assertContains('LARAVELTEST --- site1',$fileContent);
-//
-//        $process->stop(0);
-//
-//        $string = 'pkill -f "' . $processName . '"';
-//        exec($string);
-//
-//        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:remove site1.test --force');
-//        $process->run();
-//
-//
-//        $this->files->delete($fileToTest);
-//
-//        return;
-//
-//    }
+
+    /*
+    * TEST FOR QUEUE COMMANDS
+    * In this test we check that queues with the database driver refer to the correct db depending upon the domain.
+    * We use for simplicity the queue:flush command.
+    * The default .env file containts QUEUE_DRIVER=database and DB_DATABASE=homestead
+    * First we create a new domain site1.test and we update his .env with DB_DATABASE=site1 and we reset and run the migrations
+    * in the homestead db.
+    * Then, we check that the queue:flush command launched without options displays the standard success message
+    * "All failed jobs deleted successfully!" and that the queue:flush command launched with the --domain=site1.test
+    * option fails because it doesn't find the failed_job table as we did'nt migrate for that db.
+    * Last, we repeat the check with the other db.
+    *
+    */
+    public function testQueueCommand() {
+        //Note that if the $_SERVER['SERVER_NAME'] value has been set and the --domain option has NOT been set,
+        //the $_SERVER['SERVER_NAME'] value acts as the --domain option value.
+        // So all the artisan commands run as if the option were instantiated.
+        $serverName = Arr::get($_SERVER,'SERVER_NAME');
+
+
+        //ADDING DOMAIN AND UPDATING ENV
+        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:add site1.test');
+        $process->run();
+
+        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:update_env site1.test --domain_values=\'{"DB_DATABASE":"site1"}\'');
+        $process->run();
+
+
+        //RESET MIGRATIONS IN BOTH DBS
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset');
+        $process->run();
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset --domain=site1.test');
+        $process->run();
+
+        //MIGRATIONS WITHOUT DOMAIN OPTION
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate');
+        $process->run();
+//        $this->assertEquals("Laravel",$process->getOutput());
+
+
+        //CHECK QUEUE:FLUSH COMMAND: SUCCESS WITHOUT OPTIONS AND FAILURE WITH DOMAIN OPTION
+        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush');
+        $process->run();
+        $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
+
+        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush --domain=site1.test');
+        $process->run();
+        if (in_array($serverName, ['site1.test']) || Str::endsWith($serverName,'.site1.test')) {
+            $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
+        } else {
+            $this->assertContains('SQLSTATE[42S02]: Base table or view not found: 1146 Table \'site1.failed',$process->getOutput());
+        }
+
+
+
+        //RESET MIGRATIONS IN BOTH DBS
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset');
+        $process->run();
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset --domain=site1.test');
+        $process->run();
+
+        //MIGRATIONS WITH DOMAIN OPTION
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate --domain=site1.test');
+        $process->run();
+//        $this->assertEquals("Laravel",$process->getOutput());
+
+        //CHECK QUEUE:FLUSH COMMAND: SUCCESS WITH DOMAIN OPTIION AND FAILURE WITHOUT
+        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush --domain=site1.test');
+        $process->run();
+        $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
+
+        $process = new Process('php '.$this->laravelAppPath.'/artisan queue:flush');
+        $process->run();
+        if (in_array($serverName, ['site1.test']) || Str::endsWith($serverName,'.site1.test')) {
+            $this->assertContains('All failed jobs deleted successfully!',$process->getOutput());
+        } else {
+            $this->assertContains('SQLSTATE[42S02]: Base table or view not found: 1146 Table \'homestead.failed',$process->getOutput());
+        }
+
+        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:remove site1.test --force');
+        $process->run();
+
+        return;
+
+    }
+
+    /*
+       * TEST FOR QUEUE LISTEN COMMAND
+       * In this test we check that queues listeners applied to distinct domains work with the jobs pushed from
+       * the corresponding domain and that the queues where they are pushed are those configured in the .env files.
+       *
+       * The job Gecche\Multidomain\Tests\Jobs\AppNameJob simply put in a file the name of the queue and the env(APP_NAME) value.
+       * We push that job using an artisan Gecche\Multidomain\Tests\Console\Commands\QueuePush (queue_push) command which simply push the job in the queue.
+       *
+       */
+    public function testQueueListenCommand() {
+
+
+        //Note that if the $_SERVER['SERVER_NAME'] value has been set and the --domain option has NOT been set,
+        //the $_SERVER['SERVER_NAME'] value acts as the --domain option value.
+        // So all the artisan commands run as if the option were instantiated.
+        $serverName = Arr::get($_SERVER,'SERVER_NAME');
+
+
+        //ADDING DOMAIN AND UPDATING ENV
+        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:add site1.test');
+        $process->run();
+
+        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:update_env site1.test --domain_values=\'{"APP_NAME":"LARAVELTEST","DB_DATABASE":"site1","QUEUE_DEFAULT":"site1"}\'');
+        $process->run();
+
+
+        //RESET MIGRATIONS IN BOTH DBS
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset');
+        $process->run();
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate:reset --domain=site1.test');
+        $process->run();
+
+        //MIGRATIONS WITHOUT DOMAIN OPTION
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate');
+        $process->run();
+
+
+        //This the file used by the AppNameJob job
+        $fileToTest = $this->laravelAppPath.'/queueresult.txt';
+
+        $this->files->delete($fileToTest);
+
+        $this->assertFileNotExists($fileToTest);
+
+        //We start the listener
+        $processName = "php ".$this->laravelAppPath. "/artisan queue:listen";
+        $process = new Process($processName);
+        $process->start();
+
+        //We push the Job using the queue_push command
+        $process2 = new Process('php '.$this->laravelAppPath.'/artisan queue_push');
+        $process2->run();
+
+        //Wait for a reasonable time (the only way we managed to simulate queue listeners)
+        sleep(5);
+
+        //We assert that the file exists
+        $this->assertFileExists($fileToTest);
+
+        //Depending upon the domain option (or the SERVER_NAME value)
+        //we check accordingly the contents of the file
+        $fileContent = $this->files->get($fileToTest);
+        if (in_array($serverName, ['site1.test']) || Str::endsWith($serverName,'.site1.test')) {
+            $this->assertContains('LARAVELTEST --- site1',$fileContent);
+        } else {
+            $this->assertContains('Laravel --- default',$fileContent);
+        }
+
+
+        $process->stop(0);
+
+        //We kill the listener process by name
+        $string = 'pkill -f "' . $processName . '"';
+        exec($string);
+
+
+        /*
+         * We repeat the stuff under the domain "site1.test"
+         */
+        /***/
+        //MIGRATIONS WITH DOMAIN OPTION
+        $process = new Process('php '.$this->laravelAppPath.'/artisan migrate --domain=site1.test');
+        $process->run();
+
+        $this->files->delete($fileToTest);
+
+        $this->assertFileNotExists($fileToTest);
+
+        $processName = 'php '.$this->laravelAppPath.'/artisan queue:listen --domain=site1.test';
+        $process = new Process($processName);
+        $process->start();
+
+        $process2 = new Process('php '.$this->laravelAppPath.'/artisan queue_push --domain=site1.test');
+        $process2->run();
+
+        //Wait for a reasonable time
+        sleep(5);
+
+        $this->assertFileExists($fileToTest);
+
+        $fileContent = $this->files->get($fileToTest);
+        $this->assertContains('LARAVELTEST --- site1',$fileContent);
+
+        $process->stop(0);
+
+        $string = 'pkill -f "' . $processName . '"';
+        exec($string);
+
+        $process = new Process('php '.$this->laravelAppPath.'/artisan domain:remove site1.test --force');
+        $process->run();
+
+
+        $this->files->delete($fileToTest);
+
+        return;
+
+    }
 }
