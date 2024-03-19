d345201f7fc9561b2f1d7676af6193b9221173d8
Giacomo Terreni <giacomoterreni@gmail.com>
2024-03-19 16:34:26 +0100

    tests in L11

--- tests/src/HttpTestCase.php
+++ tests/src/HttpTestCase.php
@@ -230,80 +230,80 @@ class HttpTestCase extends \Orchestra\Testbench\BrowserKit\TestCase
             ->see($stringToSee);
     }
 
-//
-//    /**
-//     * In this test we checks that the database connection is versus the right database set in the
-//     * DB_DATABASE entry of the env file.
-//     * The chosen env files again depends upon the $_SERVER['SERVER_NAME'] value
-//     * set when the test has been launched.
-//     *
-//     */
-//    public function testDBConnection()
-//    {
-//
-//        $this->serverName = Arr::get($_SERVER, 'SERVER_NAME');
-//        $dbName = DB::connection('mysql')->getDatabaseName();
-//        $expectedDb = 'homestead';
-//        if (in_array($this->serverName, [$this->subSite1])) {
-//            $expectedDb = $this->subSiteDbName1;
-//        } elseif (in_array($this->serverName, [$this->site1]) || Str::endsWith($this->serverName, '.'.$this->site1)) {
-//            $expectedDb = $this->siteDbName1;
-//        } elseif (in_array($this->serverName, [$this->site2]) || Str::endsWith($this->serverName, '.'.$this->site2)) {
-//            $expectedDb = $this->siteDbName2;
-//        }
-//
-//        $this->assertEquals($expectedDb, $dbName);
-//    }
-//
-//
-//    /**
-//     * In this test we checks that the database connection is versus the right database set in the
-//     * DB_DATABASE entry of the env file.
-//     * The chosen env files again depends upon the $_SERVER['SERVER_NAME'] value
-//     * set when the test has been launched.
-//     *
-//     */
-//    public function testEnvFile()
-//    {
-//
-//        $this->serverName = Arr::get($_SERVER, 'SERVER_NAME');
-//        $envfileName = app()->environmentFile();
-//        $expectedEnvFile = '.env';
-//        if (in_array($this->serverName, [$this->subSite1])) {
-//            $expectedEnvFile = '.env.'.$this->subSite1;
-//        } elseif (in_array($this->serverName, [$this->site1]) || Str::endsWith($this->serverName, '.'.$this->site1)) {
-//            $expectedEnvFile = '.env.'.$this->site1;
-//        } elseif (in_array($this->serverName, [$this->site2]) || Str::endsWith($this->serverName, '.'.$this->site2)) {
-//            $expectedEnvFile = '.env.'.$this->site2;
-//        }
-//
-//        $this->assertEquals($expectedEnvFile, $envfileName);
-//    }
-//
-//    /**
-//     * In this test we checks that the database connection is versus the right database set in the
-//     * DB_DATABASE entry of the env file.
-//     * The chosen env files again depends upon the $_SERVER['SERVER_NAME'] value
-//     * set when the test has been launched.
-//     *
-//     */
-//    public function testStorageFolder()
-//    {
-//
-//        $this->serverName = Arr::get($_SERVER, 'SERVER_NAME');
-//        $storageFolder = storage_path();
-//        $expectedStorageFolder = base_path() . '/storage';
-//
-//        if (in_array($this->serverName, [$this->subSite1])) {
-//            $expectedStorageFolder = $expectedStorageFolder . DIRECTORY_SEPARATOR . domain_sanitized($this->subSite1);
-//        } elseif (in_array($this->serverName, [$this->site1]) || Str::endsWith($this->serverName, '.'.$this->site1)) {
-//            $expectedStorageFolder = $expectedStorageFolder . DIRECTORY_SEPARATOR . domain_sanitized($this->site1);
-//        } elseif (in_array($this->serverName, [$this->site2]) || Str::endsWith($this->serverName, '.'.$this->site2)) {
-//            $expectedStorageFolder = $expectedStorageFolder . DIRECTORY_SEPARATOR . domain_sanitized($this->site2);
-//        }
-//
-//        $this->assertEquals($expectedStorageFolder, $storageFolder);
-//    }
-//
+
+    /**
+     * In this test we checks that the database connection is versus the right database set in the
+     * DB_DATABASE entry of the env file.
+     * The chosen env files again depends upon the $_SERVER['SERVER_NAME'] value
+     * set when the test has been launched.
+     *
+     */
+    public function testDBConnection()
+    {
+
+        $this->serverName = Arr::get($_SERVER, 'SERVER_NAME');
+        $dbName = DB::connection('mysql')->getDatabaseName();
+        $expectedDb = 'homestead';
+        if (in_array($this->serverName, [$this->subSite1])) {
+            $expectedDb = $this->subSiteDbName1;
+        } elseif (in_array($this->serverName, [$this->site1]) || Str::endsWith($this->serverName, '.'.$this->site1)) {
+            $expectedDb = $this->siteDbName1;
+        } elseif (in_array($this->serverName, [$this->site2]) || Str::endsWith($this->serverName, '.'.$this->site2)) {
+            $expectedDb = $this->siteDbName2;
+        }
+
+        $this->assertEquals($expectedDb, $dbName);
+    }
+
+
+    /**
+     * In this test we checks that the database connection is versus the right database set in the
+     * DB_DATABASE entry of the env file.
+     * The chosen env files again depends upon the $_SERVER['SERVER_NAME'] value
+     * set when the test has been launched.
+     *
+     */
+    public function testEnvFile()
+    {
+
+        $this->serverName = Arr::get($_SERVER, 'SERVER_NAME');
+        $envfileName = app()->environmentFile();
+        $expectedEnvFile = '.env';
+        if (in_array($this->serverName, [$this->subSite1])) {
+            $expectedEnvFile = '.env.'.$this->subSite1;
+        } elseif (in_array($this->serverName, [$this->site1]) || Str::endsWith($this->serverName, '.'.$this->site1)) {
+            $expectedEnvFile = '.env.'.$this->site1;
+        } elseif (in_array($this->serverName, [$this->site2]) || Str::endsWith($this->serverName, '.'.$this->site2)) {
+            $expectedEnvFile = '.env.'.$this->site2;
+        }
+
+        $this->assertEquals($expectedEnvFile, $envfileName);
+    }
+
+    /**
+     * In this test we checks that the database connection is versus the right database set in the
+     * DB_DATABASE entry of the env file.
+     * The chosen env files again depends upon the $_SERVER['SERVER_NAME'] value
+     * set when the test has been launched.
+     *
+     */
+    public function testStorageFolder()
+    {
+
+        $this->serverName = Arr::get($_SERVER, 'SERVER_NAME');
+        $storageFolder = storage_path();
+        $expectedStorageFolder = base_path() . '/storage';
+
+        if (in_array($this->serverName, [$this->subSite1])) {
+            $expectedStorageFolder = $expectedStorageFolder . DIRECTORY_SEPARATOR . domain_sanitized($this->subSite1);
+        } elseif (in_array($this->serverName, [$this->site1]) || Str::endsWith($this->serverName, '.'.$this->site1)) {
+            $expectedStorageFolder = $expectedStorageFolder . DIRECTORY_SEPARATOR . domain_sanitized($this->site1);
+        } elseif (in_array($this->serverName, [$this->site2]) || Str::endsWith($this->serverName, '.'.$this->site2)) {
+            $expectedStorageFolder = $expectedStorageFolder . DIRECTORY_SEPARATOR . domain_sanitized($this->site2);
+        }
+
+        $this->assertEquals($expectedStorageFolder, $storageFolder);
+    }
+
 
 }
